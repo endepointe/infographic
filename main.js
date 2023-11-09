65f333b868268d3390fcc2aacef9230dfcff160f
endepointe <endepointe@gmail.com>
2023-11-09 10:56:31 -0800

    ssdd

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 
+const [file] = document.querySelector("input[type=file]").files;
+const reader = new FileReader();
+reader.addEventListener(
+    "load", 
+    () => {
+        console.log(reader.result.length);
+    },
+    false,
+);
+
+if (file) {
+    reader.readAsText(file);
+}
 
 // Get the path element
 const path = document.getElementById('myPath');
@@ -19,3 +32,51 @@ function drawPath() {
 // Call the drawPath function after a delay (e.g., 1 second)
 setTimeout(drawPath, 1000);
 
+class SvgPath extends HTMLElement {
+    // Define observed attributes (attributes to monitor for changes)
+    static get observedAttributes() {
+        return ['path'];
+    }
+
+    // Constructor: called when the element is created
+    constructor() {
+        super();
+        // Create a Shadow DOM
+        this.attachShadow({mode: 'open'});
+    }
+
+    // Called when the element is added to the DOM
+    connectedCallback() {
+        this.render();
+    }
+
+    // Called when observed attributes change
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'path' && oldValue !== newValue) {
+            this.render();
+        }
+    }
+
+    // Render the SVG based on the 'path' attribute
+    render() {
+        // Get the 'path' attribute value
+        const pathString = this.getAttribute('path') || ''; 
+
+        // Create SVG and path elements
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        // Set the path attribute based on the 'path' attribute value
+        path.setAttribute('d', pathString);
+
+        // Append the path to the SVG
+        svg.appendChild(path);
+
+        // Append the SVG to the Shadow DOM
+        this.shadowRoot.innerHTML = '';
+        this.shadowRoot.appendChild(svg);
+    }
+}
+
+// Define the custom element 'svg-path'
+customElements.define('svg-path', SvgPath);
